494ddde2c0630353d1d8a30c9a3cf7e570936177
jone <jone.liu@etocrm.com>
2016-11-10 18:27:51 +0800

    添加自动播放音乐

---
+++ autoplayMusic/play.py
@@ -0,0 +1,18 @@
+# -*- coding:utf8 -*-
+
+import os , random
+
+
+music_list = os.path.join(os.path.abspath(os.path.curdir), 'music_list.lst')
+music_dir = os.path.join(os.path.abspath(os.path.curdir), 'music')
+
+
+if os.path.exists(music_list):
+    os.remove(music_list)
+
+all_music = [os.path.join(music_dir, music) for music in os.listdir(music_dir)]
+random.shuffle(all_music)
+
+content = '\n'.join(all_music)
+
+open(music_list, 'w').write(content)
